4c182cfbe91a711cab29aa029494f42a2ddb194b
Apolo Freitas <apolobrener11@gmail.com>
2021-01-07 16:08:11 -0400

    feat: ship shooting

--- src/objects/ShipObject.ts
+++ src/objects/ShipObject.ts
@@ -1,19 +1,25 @@
 import { shipSprite } from "~/assets";
 
 enum Tilting {
-  "false",
-  "left",
-  "right",
+  false,
+  left,
+  right,
 }
 enum Moving {
-  "false",
-  "forward",
+  false,
+  forward,
+}
+enum Shooting {
+  ready,
+  shooting,
+  delay,
 }
 
 export class ShipObject extends Phaser.Physics.Arcade.Sprite {
   public commands = {
     moving: Moving.false,
     tilting: Tilting.false,
+    shooting: Shooting.ready,
   };
   constructor(public scene: Phaser.Scene, x?: number, y?: number) {
     super(scene, x, y, shipSprite);
@@ -27,37 +33,32 @@ export class ShipObject extends Phaser.Physics.Arcade.Sprite {
   }
 
   preUpdate(time: number, delta: number) {
+    this.wrapOnWorldBounds();
     this.updateCommands();
+    this.updateObject();
+  }
 
-    if (this.commands.moving === Moving.forward) {
-      const rotation =
-        this.rotation <= 0 ? -this.rotation : -this.rotation + 2 * Math.PI;
-      this.setAcceleration(Math.cos(rotation) * 100, -Math.sin(rotation) * 100);
-    }
-    if (this.commands.moving === Moving.false) {
-      this.setAcceleration(0, 0);
-    }
+  wrapOnWorldBounds() {
+    const { bounds } = this.scene.physics.world;
 
-    if (this.commands.tilting === Tilting.left) {
-      this.setAngularVelocity(-200);
+    if (this.x < bounds.x - this.width / 2) {
+      this.x = bounds.x + bounds.width + this.width / 2;
     }
-    if (this.commands.tilting === Tilting.right) {
-      this.setAngularVelocity(200);
+    if (this.x > bounds.x + bounds.width + this.width / 2) {
+      this.x = bounds.x - this.width / 2;
     }
-    if (this.commands.tilting === Tilting.false) {
-      this.setAngularVelocity(0);
+    if (this.y < bounds.y - this.height / 2) {
+      this.y = bounds.y + bounds.height + this.height / 2;
     }
-
-    const { main: camera } = this.scene.cameras;
-
-    this.x = this.x < 0 - this.width / 2 ? camera.x + camera.width : this.x;
-    this.x = this.x > camera.x + camera.width + this.width / 2 ? 0 : this.x;
-    this.y = this.y < 0 - this.height / 2 ? camera.y + camera.height : this.y;
-    this.y = this.y > camera.y + camera.height + this.height / 2 ? 0 : this.y;
+    if (this.y > bounds.y + bounds.height + this.height / 2) {
+      this.y = bounds.y - this.height / 2;
+    }
   }
 
   updateCommands() {
-    const { W, A, D } = this.scene.input.keyboard.addKeys("W,A,D") as {
+    const { W, A, D, SPACE } = this.scene.input.keyboard.addKeys(
+      "W,A,D,SPACE"
+    ) as {
       [key: string]: Phaser.Input.Keyboard.Key;
     };
 
@@ -67,5 +68,32 @@ export class ShipObject extends Phaser.Physics.Arcade.Sprite {
 
     if (W.isDown) this.commands.moving = Moving.forward;
     if (!W.isDown) this.commands.moving = Moving.false;
+
+    if (SPACE.isDown && this.commands.shooting === Shooting.ready)
+      this.commands.shooting = Shooting.shooting;
+  }
+
+  updateObject() {
+    if (this.commands.moving === Moving.forward) {
+      const rotation = -this.rotation + (this.rotation < 0 ? 2 * Math.PI : 0);
+      this.setAcceleration(Math.cos(rotation) * 100, -Math.sin(rotation) * 100);
+    }
+    if (this.commands.moving === Moving.false) {
+      this.setAcceleration(0, 0);
+    }
+    if (this.commands.tilting === Tilting.left) {
+      this.setAngularVelocity(-200);
+    }
+    if (this.commands.tilting === Tilting.right) {
+      this.setAngularVelocity(200);
+    }
+    if (this.commands.tilting === Tilting.false) {
+      this.setAngularVelocity(0);
+    }
+    if (this.commands.shooting === Shooting.shooting) {
+      console.log("shot");
+      this.commands.shooting = Shooting.delay;
+      setTimeout(() => (this.commands.shooting = Shooting.ready), 250);
+    }
   }
 }
--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -3,35 +3,27 @@ import { ShipObject } from "~/objects/ShipObject";
 
 export class GameScene extends Phaser.Scene {
   constructor() {
-    super({
-      key: "GameScene",
-      plugins: {
-        wrap: {
-          min: {
-            x: 0,
-            y: 0,
-          },
-          max: {
-            x: 500,
-            y: 500,
-          },
-        },
-      },
-    });
+    super({ key: "GameScene" });
   }
 
   create() {
-    const bg = this.add.tileSprite(
-      this.cameras.main.width / 2,
-      this.cameras.main.height / 2,
+    this.physics.world.setBounds(
+      0,
+      0,
       this.cameras.main.width,
-      this.cameras.main.height,
+      this.cameras.main.height
+    );
+    const bg = this.add.tileSprite(
+      this.physics.world.bounds.width / 2,
+      this.physics.world.bounds.height / 2,
+      this.physics.world.bounds.width,
+      this.physics.world.bounds.height,
       backgroundSprite
     );
     const ship = new ShipObject(
       this,
-      this.cameras.main.width / 2,
-      this.cameras.main.height / 2
+      this.physics.world.bounds.width / 2,
+      this.physics.world.bounds.height / 2
     );
   }
 
